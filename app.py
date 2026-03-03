7b5322680f1ed59e39e34caac5a2286489f1d723
Manny <Mannyiheukwumere@gmail.com>
2026-03-03 11:59:20 +0100

    Learnt how print works

---
+++ app.py
@@ -0,0 +1,11 @@
+# This is just a comment
+# This is another comment
+# This is yet another
+
+
+trousers_quantity = 3 # number of trousers bought
+print(trousers_quantity,"trousers")
+trouser_cost = 750 # cost of one trouser
+print("Each trouser costs",trouser_cost)
+total_cost = trouser_cost * trousers_quantity # total amount paid for trousers
+print("Total cost of trousers",total_cost)
