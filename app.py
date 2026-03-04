2e1be72bef60d91b2b72d766b68786c9e9a5d7cd
Manny <Mannyiheukwumere@gmail.com>
2026-03-04 12:01:51 +0100

    Introduction to Python input function

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 # print("Mary is learning Python")
 # print("Mary wants to become a Python Expert")
 
-name ="Manny"
-language = "C#"
+name = input("Enter your name: ")
+language = input("Preferred language: ")
 print("My name is",name)
 print(name,"is learning",language)
 print(name,"wants to become a",language,"Expert")
