cac75e0b285d5eb0d9acd10c7a7225e4d8c1622a
Manny <Mannyiheukwumere@gmail.com>
2026-03-11 12:10:25 +0100

    Used the format (f) string to join strings

--- strings101.py
+++ strings101.py
@@ -1,24 +1,20 @@
+# sentence = """I love Python.
+# Python is very easy, Python is also very
+# powerful. AI depends mostly on python.
+# Python can be used for.
+# - AI
+# - Web Design
+# - Automations
+# Python is a highly versatile language
+# """
+# print(sentence.replace('Python','Java').replace('python','Java'))
+
+# eurodate = "2026/03/11"
+# print(eurodate.replace("/","-"))
 name = "Manny"
-surname = "Ezeagwulanganna"
-age = 21
+surname = "Eze"
+age = 34
 
-# #print("My name is ",name,"I am",age,"years old")
-# print(name+' '+ str(age))
-
-# isstudent = True
-# print(f"{{isstudent}} variable contains {len(str(isstudent))} characters")
-
-# Math String operation
-sentence = """I love Python.
-Python is very easy, Python is also very
-powerful. AI depends mostly on python.
-Python can be used for.
-- AI
-- Web Design
-- Automations
-Python is a highly versatile language
-"""
-
-#print(sentence)
-
-print(f" The word 'Python' occurs {sentence.count('Python')} times in the  above sentence.")
+fullname =name+' '+surname
+# print("My name is ",fullname+" i am "+str(age) + " years old")
+print(f"My name is {fullname}.I am {age} years old.\nIn the next ten year, i will be {age+10} years old")
