05be506deb789945af128e2eae49b8e48963802d
Manny <Mannyiheukwumere@gmail.com>
2026-03-11 10:47:14 +0100

    Finalizing data types

---
+++ data_types.py
@@ -0,0 +1,24 @@
+a = "Hello"
+b = 'Emmanuel'
+c = 16
+d = 3.56
+e= """198"""
+f = True
+g = [1,2,"Emily",True]
+h = None
+
+# print(b.upper())
+# print(b.lower())
+print(c.bit_length())
+
+
+# print(f"This variable contains {len(g)} items")
+#print(f"This variable contains {abs(c)} items")
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+# print(type(h))
