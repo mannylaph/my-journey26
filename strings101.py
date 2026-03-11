590a049e4ad67848b7d4d5ce62d9a5d5607254c5
Manny <Mannyiheukwumere@gmail.com>
2026-03-11 11:16:17 +0100

    Learning the type(), str(), and count() string functions

---
+++ strings101.py
@@ -0,0 +1,24 @@
+name = "Manny"
+surname = "Ezeagwulanganna"
+age = 21
+
+# #print("My name is ",name,"I am",age,"years old")
+# print(name+' '+ str(age))
+
+# isstudent = True
+# print(f"{{isstudent}} variable contains {len(str(isstudent))} characters")
+
+# Math String operation
+sentence = """I love Python.
+Python is very easy, Python is also very
+powerful. AI depends mostly on python.
+Python can be used for.
+- AI
+- Web Design
+- Automations
+Python is a highly versatile language
+"""
+
+#print(sentence)
+
+print(f" The word 'Python' occurs {sentence.count('Python')} times in the  above sentence.")
