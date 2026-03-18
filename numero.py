b77b69fbf6e05ff5c4a2c064fe921de1554c2272
Manny <Mannyiheukwumere@gmail.com>
2026-03-18 10:58:59 +0100

    Python numeric data 101

---
+++ numero.py
@@ -0,0 +1,37 @@
+import math as m, random as r
+# x = 4
+# y = 5.6
+# advanced = 3 * 4j
+
+# print(type(x))
+# print(type(y))
+# print(type(advanced))
+
+
+# print(float(x))
+# print(int(y))
+# print(complex(x,y))
+
+# print(2+4)
+# print(4-3)
+longNum = 40/6
+# print(40//6)
+# print(7%3)
+# print(3**2)
+
+# distance = 10-32
+# print(distance)
+# print(abs(distance))
+# print(longNum)
+# number = 144000
+
+# print(m.ceil(longNum))
+# print(m.floor(longNum))
+# print(m.trunc(longNum))
+# print(int(longNum))
+# print(m.floor(m.sqrt(number)))
+name = "john"
+num1 = r.random()
+#num2 =r.randint(18,30)
+print(type(num1))
+print(isinstance(name,float))
